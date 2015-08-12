a0621475307d8a93df7803b6c8cec940f53fce74
Bogdan Chadkin <trysound@yandex.ru>
2015-08-12 15:49:11 +0300

    Less decl iterations

--- index.js
+++ index.js
@@ -9,26 +9,25 @@ module.exports = postcss.plugin('postcss-opacity', function (opts) {
   return function (css) {
 
     css.eachRule(function (rule) {
-      rule.eachDecl(PROP, function (decl) {
 
-        // get amount and create value
-        var amount = Math.floor(decl.value * 100);
-        var VAL_REPLACE  = '"progid:DXImageTransform.Microsoft.Alpha(Opacity=' + amount + ')"';
-
-        // find if a filter opacity is already present
-        var isFilterAlreadyPresent = false;
-        rule.eachDecl(PROP_REPLACE, function (d) {
-          if (d.value === VAL_REPLACE) {
-            isFilterAlreadyPresent = true;
-          }
-        });
-
-        // adds new property only if it's not present yet
-        if (!isFilterAlreadyPresent) {
-          rule.insertAfter(decl, {prop: PROP_REPLACE, value: VAL_REPLACE});
-        }
-
+      // find if a filter opacity is already present
+      var isFilterAlreadyPresent = false;
+      rule.eachDecl(PROP_REPLACE, function () {
+        isFilterAlreadyPresent = true;
       });
+
+      if (!isFilterAlreadyPresent) {
+        rule.eachDecl(PROP, function (decl) {
+
+          // get amount and create value
+          var amount = Math.floor(decl.value * 100);
+          var VAL_REPLACE  = '"progid:DXImageTransform.Microsoft.Alpha(Opacity=' + amount + ')"';
+
+          // adds new property only if it's not present yet
+          rule.insertAfter(decl, {prop: PROP_REPLACE, value: VAL_REPLACE});
+
+        });
+      }
     });
 
   };
