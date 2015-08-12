1cc8f736e8af24202223e3a31b5f1ac991202a9a
Bogdan Chadkin <trysound@yandex.ru>
2015-08-12 15:44:23 +0300

    Upgrade to PostCSS 4.1.x

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-module.exports = function (opts) {
+var postcss = require('postcss');
+
+module.exports = postcss.plugin('postcss-opacity', function (opts) {
   opts = opts || {};
 
   var PROP         = 'opacity';
@@ -7,9 +9,7 @@ module.exports = function (opts) {
   return function (css) {
 
     css.eachRule(function (rule) {
-      rule.eachDecl(function (decl) {
-
-        if (decl.prop !== PROP) { return; }
+      rule.eachDecl(PROP, function (decl) {
 
         // get amount and create value
         var amount = Math.floor(decl.value * 100);
@@ -17,8 +17,8 @@ module.exports = function (opts) {
 
         // find if a filter opacity is already present
         var isFilterAlreadyPresent = false;
-        rule.eachDecl(function (d) {
-          if (d.prop === PROP_REPLACE && d.value === VAL_REPLACE) {
+        rule.eachDecl(PROP_REPLACE, function (d) {
+          if (d.value === VAL_REPLACE) {
             isFilterAlreadyPresent = true;
           }
         });
@@ -32,7 +32,4 @@ module.exports = function (opts) {
     });
 
   };
-};
-module.exports.postcss = function (css) {
-  return module.exports()(css);
-};
+});
